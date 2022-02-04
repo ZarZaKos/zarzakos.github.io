78e15634f7ca4f1dac6ff93bb5a0c33cb5b169f7
ZarZaKos <zarzakos@gmail.com>
2022-02-04 19:05:22 -0300

    PWA test

---
+++ precache-manifest.d8fafe75c339494e974f846278e58479.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d99164242fff79812e",
+    "url": "/css/app.0a4e2b28.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a18f5f4b09b86f471c0c4cffc1b2cb1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf0973096ae35641c2bb",
+    "url": "/js/about.f16125eb.js"
+  },
+  {
+    "revision": "02d99164242fff79812e",
+    "url": "/js/app.a5df98ac.js"
+  },
+  {
+    "revision": "36c0ef114def8a28a887",
+    "url": "/js/chunk-vendors.005dc150.js"
+  },
+  {
+    "revision": "9fe1a4623746e0ccd591574f4daa5397",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
